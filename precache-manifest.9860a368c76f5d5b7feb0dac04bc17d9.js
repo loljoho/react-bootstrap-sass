8bbb49a0cbe8f6a10bd4ac0a1429857b734576a3
loljoho <loljoho@users.noreply.github.com>
2019-12-14 12:19:25 -0500

    Updates

---
+++ precache-manifest.9860a368c76f5d5b7feb0dac04bc17d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb22e614c837d847d75e77a1ec6ab370",
+    "url": "/react-bootstrap-sass/index.html"
+  },
+  {
+    "revision": "d18559d738f26f94ce76",
+    "url": "/react-bootstrap-sass/static/css/main.1ef0a131.chunk.css"
+  },
+  {
+    "revision": "7480d60ca89e612385fa",
+    "url": "/react-bootstrap-sass/static/js/2.662a4f25.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-bootstrap-sass/static/js/2.662a4f25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d18559d738f26f94ce76",
+    "url": "/react-bootstrap-sass/static/js/main.ee56dd13.chunk.js"
+  },
+  {
+    "revision": "ece46214495d5966b977",
+    "url": "/react-bootstrap-sass/static/js/runtime-main.161c96d3.js"
+  }
+]);
